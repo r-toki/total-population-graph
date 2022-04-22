3bb4b319dc538dc0c5ed4e95f370fa6edfa077f0
rtokkie <natto.and.eggs@gmail.com>
2022-04-22 12:16:46 +0900

    printWidth: 120 に変更

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 - その他
   - firebase-tools@10 を使用
   - eslint と prettier の設定の参考
-    https://github.com/prettier/prettier/blob/554b15473dd4032a036d7db91a8f579e624c9822/docs/integrating-with-linters.md
+    - https://github.com/prettier/prettier/blob/554b15473dd4032a036d7db91a8f579e624c9822/docs/integrating-with-linters.md
+    - https://github.com/prettier/eslint-plugin-prettier#recommended-configuration
--- src/others/resas-client.ts
+++ src/others/resas-client.ts
@@ -3,13 +3,11 @@ import { Pref } from "../types";
 const API_URL = "https://opendata.resas-portal.go.jp/";
 const headers = { "X-API-KEY": import.meta.env.VITE_APP_RESAS_API_KEY };
 
-const appFetch = <T>(endpoint: string): Promise<T> =>
-  fetch(API_URL + endpoint, { headers }).then((res) => res.json());
+const appFetch = <T>(endpoint: string): Promise<T> => fetch(API_URL + endpoint, { headers }).then((res) => res.json());
 
 type FetchPrefecturesReturn = {
   message: null;
   result: Pref[];
 };
 
-export const fetchPrefectures = () =>
-  appFetch<FetchPrefecturesReturn>("api/v1/prefectures");
+export const fetchPrefectures = () => appFetch<FetchPrefecturesReturn>("api/v1/prefectures");
